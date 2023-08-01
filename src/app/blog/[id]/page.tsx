2ef68aaf6f07376d5f4e545e6c3eecb1b18da073
Dhiraj kumar Nayak <dnayak696@gmail.com>
2023-08-01 10:24:17 +0530

    comment func added

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,6 +1,7 @@
+"use client"
 import Header from '@/app/components/header';
 import data from '../../../redux/MOCK_DATA.json';
-
+import { useState} from 'react';
 
 
 
@@ -20,9 +21,9 @@ export function getPost(id: string): any{
 
 export default function BlogPostPage({params}: any) {
 
-  // const [comments, setComments] = useState([]);
+  const [comments, setComments]: any = useState([]);
 
-  // const [input , setInput] = useState();
+  const [input , setInput] = useState('')
 
   const post = getPost(params.id);
 
@@ -37,16 +38,34 @@ export default function BlogPostPage({params}: any) {
         <p className="text-gray-500 text-sm">{post.createdAt}</p>
         <p className="mt-4">{post.content}</p>
       </div>
+
+        {
+          comments.map((comment: any) =>(<div className="flex p-2 items-start space-x-2">
+          <img src={"https://picsum.photos/50/50"} alt={comment.user} className="w-12 h-12 rounded-full" />
+          <div className="flex-1">
+            <h4 className="font-bold text-md">{comment.user}</h4>
+            <p>{comment.comment}</p>
+          </div>
+        </div>))
+        }
+
       <div className="px-4 py-6 bg-gray-100 rounded-b-lg">
         <textarea
           className="w-full p-2 mb-4 bg-white border rounded"
           rows={4}
           placeholder="Leave a comment..."
+          value={input}
+          onChange={(e) => setInput(e.target.value) }
         />
-        <button  className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+        <button  
+         onClick={() => {
+          setComments([...comments, { user: "Random User", comment: input}])
+           setInput('')
+        }}
+        className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
           Comment
         </button>
       </div>
-     </div>
+    </div>
       </div>)
   }

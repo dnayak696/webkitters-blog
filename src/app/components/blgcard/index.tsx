eda1ce75fdfb8d3eea825c780be4845aa3e06a68
Dhiraj kumar Nayak <dnayak696@gmail.com>
2023-08-01 10:45:34 +0530

    color added to the blog card

--- src/app/components/blgcard/index.tsx
+++ src/app/components/blgcard/index.tsx
@@ -1,6 +1,6 @@
 const BlogCard = ({ title, createdAt, content, imageUrl }: any) => {
     return (
-      <div className="p-4 border rounded-lg">
+      <div className="p-4 border rounded-lg bg-white">
         <img src={imageUrl} alt={title} className="w-full h-40 object-cover rounded-t-lg" />
         <h3 className="text-lg font-semibold my-2">{title}</h3>
         <p className="text-sm text-gray-500 mb-2">{createdAt}</p>
